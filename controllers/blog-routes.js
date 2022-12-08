e29753dff3222459c11d8c1e4bfd944829fff737
Alice Shao <a1ice_shao@yahoo.com>
2022-12-08 02:18:39 -0500

    added jawsdb connection

--- controllers/blog-routes.js
+++ controllers/blog-routes.js
@@ -10,4 +10,13 @@ router.get("/", async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
+
+router.post('/', async(req, res) => {
+    try {
+      const newBlog = await Blog.create(req.body);
+      res.status(200).json(newBlog);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
